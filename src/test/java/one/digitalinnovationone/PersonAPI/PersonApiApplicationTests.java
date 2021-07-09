c7d43941d1a59331f3b22541a8da3939d1eed71b
lucaslppoi <lucaslorenzaop@gmail.com>
2021-07-09 12:48:46 -0300

    Working Starter Files

---
+++ src/test/java/one/digitalinnovationone/PersonAPI/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovationone.PersonAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
