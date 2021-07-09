70c31899cb606abc9efbd0f57221fb1195fb7d39
lucaslppoi <lucaslorenzaop@gmail.com>
2021-07-09 15:47:51 -0300

    Developed Half

---
+++ src/main/java/one/digitalinnovationone/PersonAPI/dto/mapper/PersonMapper.java
@@ -0,0 +1,16 @@
+package one.digitalinnovationone.PersonAPI.dto.mapper;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entities.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+
+@Mapper(componentModel = "spring")
+public interface PersonMapper {
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO dto);
+
+    PersonDTO toDTO(Person dto);
+}
