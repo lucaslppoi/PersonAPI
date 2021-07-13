994350fddb130f6839bba20963e9a41b26ea6c92
lucaslppoi <lucaslorenzaop@gmail.com>
2021-07-12 21:33:06 -0300

    All Included

--- src/main/java/one/digitalinnovationone/PersonAPI/service/PersonService.java
+++ src/main/java/one/digitalinnovationone/PersonAPI/service/PersonService.java
@@ -9,11 +9,8 @@ import one.digitalinnovationone.PersonAPI.exception.PersonNotFoundException;
 import one.digitalinnovationone.PersonAPI.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
